ca92292a6548a76383575f50fb61b17fac8ef89c
rseders <roxanne.seders@gmail.com>
2018-01-22 14:18:33 -0700

    api linking

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
-import Header from "../comps/Header";
 import Layout from "../comps/MyLayout.js";
-//added 1/22
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
-
-export default () => (
+const Index = props => (
   <Layout>
     <h1>CHEERS</h1>
     <ul>
-      <PostLink title="About" />
-      <PostLink title="Bars" />
-      <PostLink title="Sign in" />
+      {props.shows.map(({ show }) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=cheers");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  };
+};
+
+export default Index;
